99e478c1723562ce2c0f5bff7926c578ff9bb350
dioufcodeur <modoudiouf535@gmail.com>
2023-01-31 22:45:40 +0000

    reservoir project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-test-30-01';
+  color = 'red';
+
+
+  reservoirs: reservoirClass[] = [
+    new reservoirClass(23),
+    new reservoirClass(53),
+    new reservoirClass(35),
+    new reservoirClass(88),
+    new reservoirClass(3),
+    new reservoirClass(40),
+    new reservoirClass(12),
+    new reservoirClass(59)
+  ];
+
+  changeTextAndColor(indexReservoir: number) {
+    if (this.reservoirs[indexReservoir].percentReservoir == 100) {
+      this.reservoirs[indexReservoir].percentReservoir = 100;
+      return;
+    }
+    this.reservoirs[indexReservoir].percentReservoir = this.reservoirs[indexReservoir].percentReservoir + 1;
+    this.color = 'green';
+  }
+
+}
+
+export class reservoirClass {
+  percentReservoir: number;
+
+  constructor(percentReservoir: number) {
+    this.percentReservoir = percentReservoir;
+  }
 }
